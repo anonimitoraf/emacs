8c87d8fa29a29f879a4474498c2b672f069b713b
Rafael Nicdao <nicdaoraf@gmail.com>
2022-08-13 12:58:18 +1000

    Add vertico and orderless

--- init.el
+++ init.el
@@ -21,9 +21,55 @@
 (setq package-enable-at-startup nil)
 
 ;; Install packages
-(setq ++packages '(use-package evil))
-(dolist (++p ++packages)
-  (straight-use-package ++p))
+(setq +packages '(use-package evil vertico orderless))
+(dolist (+p +packages)
+  (straight-use-package +p))
 
+;; --- Evil ---
 (use-package evil
   :config (evil-mode t))
+
+;; --- Vertico ---
+(use-package vertico
+  :config (vertico-mode t))
+;; A few more useful configurations...
+(use-package emacs
+  :init
+  ;; Add prompt indicator to `completing-read-multiple'.
+  ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
+  (defun crm-indicator (args)
+    (cons (format "[CRM%s] %s"
+                  (replace-regexp-in-string
+                   "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+                   crm-separator)
+                  (car args))
+          (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+  ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
+  ;; Vertico commands are hidden in normal buffers.
+  ;; (setq read-extended-command-predicate
+  ;;       #'command-completion-default-include-p)
+
+  ;; Enable recursive minibuffers
+  (setq enable-recursive-minibuffers t)
+
+  (setq read-file-name-completion-ignore-case t
+      read-buffer-completion-ignore-case t
+      completion-ignore-case t))
+
+;; --- Orderless ---
+;; Optionally use the `orderless' completion style.
+(use-package orderless
+  :init
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (setq orderless-style-dispatchers '(+orderless-dispatch)
+  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
