9ccb9e2a84e2b29696c176b9130ab5ad46ecc357
Rafael Nicdao <nicdaoraf@gmail.com>
2022-08-13 13:00:17 +1000

    Add TODOs

--- init.el
+++ init.el
@@ -73,3 +73,12 @@
   (setq completion-styles '(orderless basic)
         completion-category-defaults nil
         completion-category-overrides '((file (styles partial-completion)))))
+
+;; TODOs
+;; - LSP
+;; - Marginalia
+;; - Theme
+;; - Keybindings (SPC leader)
+;; - Better jumper
+;; - Magit
+;; - Org
